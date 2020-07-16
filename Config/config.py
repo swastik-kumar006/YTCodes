22eb74dbd25cddb2dc924d9538d46493eb976c47
TERMUXID7 <55914808+BLUE-DEVIL1134@users.noreply.github.com>
2020-07-16 13:13:53 +0530

    Update config.py

--- Config/config.py
+++ Config/config.py
@@ -2,15 +2,15 @@ from time import sleep as s
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 CHROME_DRIVER = '/app/.chromedriver/bin/chromedriver'
-from Config import ALIVE_NAME
+from YTLink import Link
 i = 1
 chrome_options = Options()
 prefs = {"download.default_directory": "./chromedriver.exe"}
 chrome_options.add_argument("--window-size=1927x1083")
 chrome_options.add_experimental_option("prefs", prefs)
 driver = webdriver.Chrome(executable_path=CHROME_DRIVER)
-while i <= 5000:
+while i <= 500000:
     print(f"Sent Views : {i}")
-    driver.get(ALIVE_NAME)
-    s(200)
+    driver.get(str(Link))
+    s(250)
     i += 1
