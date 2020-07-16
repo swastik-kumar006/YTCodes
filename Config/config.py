ce587082c0d2ae0c502a00db0f8876870af8315e
TERMUXID7 <55914808+BLUE-DEVIL1134@users.noreply.github.com>
2020-07-16 08:13:01 +0530

    Update config.py

--- Config/config.py
+++ Config/config.py
@@ -1,15 +1,16 @@
 from time import sleep as s
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-CHROME_DRIVER = './'
+CHROME_DRIVER = '/app/.chromedriver/bin/chromedriver'
+from Config import ALIVE_NAME
 i = 1
 chrome_options = Options()
 prefs = {"download.default_directory": "./chromedriver.exe"}
 chrome_options.add_argument("--window-size=1927x1083")
 chrome_options.add_experimental_option("prefs", prefs)
 driver = webdriver.Chrome(executable_path=CHROME_DRIVER)
-while i <= 500:
+while i <= 5000:
     print(f"Sent Views : {i}")
-    driver.get('https://youtu.be/LAH2PEfcdIc')
+    driver.get(ALIVE_NAME)
     s(200)
     i += 1
