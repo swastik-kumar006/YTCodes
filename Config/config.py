d66d4728cc6e58313753a1468c5778b158181e8a
TERMUXID7 <55914808+BLUE-DEVIL1134@users.noreply.github.com>
2020-07-16 14:25:23 +0530

    Update config.py

--- Config/config.py
+++ Config/config.py
@@ -2,7 +2,6 @@ from time import sleep as s
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 CHROME_DRIVER = '/app/.chromedriver/bin/chromedriver'
-from YTLink import Link
 i = 1
 chrome_options = Options()
 prefs = {"download.default_directory": "./chromedriver.exe"}
@@ -11,6 +10,6 @@ chrome_options.add_experimental_option("prefs", prefs)
 driver = webdriver.Chrome(executable_path=CHROME_DRIVER)
 while i <= 500000:
     print(f"Sent Views : {i}")
-    driver.get(str(Link))
+    driver.get('https://youtu.be/qUlHZt5kYLI')
     s(250)
     i += 1
